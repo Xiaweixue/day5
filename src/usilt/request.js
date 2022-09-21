58769a7ef72d4a5edd1c5c9ffbe542f832d2efbb
“Xiaweixue” <2742377217@qq.com>
2022-09-21 10:45:23 +0800

    更新了axios的二次封装

--- src/usilt/request.js
+++ src/usilt/request.js
@@ -1,27 +1,79 @@
+
+// 引入axios
 import axios from 'axios'
-const http = axios.create({
-    baseURL: process.env.VUE_APP_BASE_API,
-    timeout: '1000'
+import { Message } from "element-ui"
+// 自定义错误信息提示内容
+const exceptionMessage = {
+    1000: '用户名或者密码发生错误',
+    3000: ''
+}
+
+
+
+//创建axios实例配置, 返回实例对象
+const service = axios.create({
+    baseURL : process.env.VUE_APP_BASE_API,
+    timeout: 5000
 })
-http.interceptors.request.use(function (config) {
-    config.headers.token = window.sessionStorage.getItem('token')
-    return config
-}), function (error) {
-    return Promise.reject(error)
-}
 
-http.interceptors.request.use(function (response) {
-    return response
-}), function (error) {
-    return Promise.reject(error)
-}
+// 请求拦截器
+service.interceptors.request.use(function (config) {
+    return config;
+}, function (error) {
+    return Promise.reject(error);
+})
 
-const service = (options) => {
-    options.method = options.method || 'get'
-    if (options.method.toLowerCase() === 'get') {
-        options.params = options.data || options.params
+// 响应拦截器
+service.interceptors.response.use(function (response) {
+
+    if (response.status < 400) {
+        //失败
+        _showError(response.data.code, response.data.message)
+        //成功
+        return response.data.data
+
     }
-    return http(options)
+
+    if (response.status === 401) {
+        // TODO token的过期处理
+        return
+    }
+
+
+
+
+    return response;
+}, function (error) {
+    return Promise.reject(error);
+})
+
+/**
+ * 错误提示
+ */
+const _showError = (errorCode, message) => {
+    let title
+    title = exceptionMessage[errorCode] || message || '发生未知错误'
+    Message.error(title)
 }
 
-export default service
+/**
+ * 解决不同请求方式时统一使用data来进行传参
+ * @param options
+ */
+const request = (options) => {
+
+    options.method = options.method || 'get'
+    if (options.method.toLowerCase() === "get") {
+        options.params = options.data || options.params
+        delete options.data
+    }
+
+    // 解决多个代理请求的问题
+    service.defaults.baseURL = options.proxy || process.env.VUE_APP_BASE_API
+
+    return service(options)
+}
+
+
+// 导出实例对象
+export default request
